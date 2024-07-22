a9a35c1bba5fbff1e2fb97ae534ee894ade2238a
gayatri-salve <salvegayatri2002@gmail.com>
2024-07-22 15:51:40 +0530

    Add files via upload
    program to sum all the items in a list.

---
+++ countlist.py
@@ -0,0 +1,16 @@
+
+#. Write a Python program to sum all the items in a list.
+# Example list
+my_list = [1, 2, 3, 4, 5]
+
+# Summing the items in the list
+result = sum(my_list)
+
+# Printing the result
+print("The sum of the list is:", result)
+
+
+
+
+#output
+#The sum of the list is: 15

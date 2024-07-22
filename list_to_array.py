749f49580fafd94ac213624000348aec7613c4d1
gayatri-salve <salvegayatri2002@gmail.com>
2024-07-22 15:52:33 +0530

    Add files via upload
    Convert the list into numpy array then display the array

---
+++ list_to_array.py
@@ -0,0 +1,20 @@
+#Convert the below list into numpy array then display the array
+
+ #Input: my_list = [1, 2, 3, 4, 5] 
+
+
+#import the numpy lib
+import numpy as np
+
+list=[1,2,3,4,5]
+
+#convert list into array
+my_array=np.array(list)
+
+print("After converting into array:",my_array)
+
+
+
+#Output
+
+After converting into array: [1 2 3 4 5]
